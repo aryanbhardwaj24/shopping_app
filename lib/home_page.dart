0ff4ad06126918d0574b84f160f3bce70dacecca
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-25 15:29:18 +0530

    store the attributes of custom border in a separate variable named border in order to make the code modular and update the same in enabledBorder and focusedBorder property of TextField in custom header

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
+
     return const Scaffold(
       body: SafeArea(
         child: Column(
@@ -26,14 +35,9 @@ class HomePage extends StatelessWidget {
                     decoration: InputDecoration(
                       hintText: 'Search',
                       prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromRGBO(225, 225, 225, 1),
-                        ),
-                        borderRadius: BorderRadius.horizontal(
-                          left: Radius.circular(50),
-                        ),
-                      ),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
                     ),
                   ),
                 ),
