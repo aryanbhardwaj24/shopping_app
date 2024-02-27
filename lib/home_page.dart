ec295e29ae5ba27cb48dff359efa60952f67281c
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-27 07:57:19 +0530

    add starter code for displaying products list on home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/global_variables.dart';
+import 'package:shopping_app/product_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -100,6 +102,12 @@ class _HomePageState extends State<HomePage> {
                   );
                 },
               ),
+            ),
+            ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+              },
             ),
           ],
         ),
---
+++ lib/product_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromRGBO(216, 240, 253, 1),
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
