fee592ce659bdcea03909e8928c7380ea092c261
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-27 10:35:55 +0530

    override the default textTheme.titleMedium and textTheme.bodySmall properties of theme in MyApp class for it to be consistent throughout the app and update the same in ProductCard class for title and price respectively

--- lib/product_card.dart
+++ lib/product_card.dart
@@ -18,11 +18,17 @@ class ProductCard extends StatelessWidget {
       color: const Color.fromRGBO(216, 240, 253, 1),
       child: Column(
         children: [
-          Text(title),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
           const SizedBox(
             height: 5,
           ),
-          Text('\$$price'),
+          Text(
+            '\$$price',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
           const SizedBox(
             height: 5,
           ),
