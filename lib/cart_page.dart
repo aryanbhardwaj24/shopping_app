a40d42c4bd529e27901064ca027d4eebba01a9d4
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-29 12:45:26 +0530

    design CartPage using ListView.builder widget returning ListTile widget from the itemBuilder

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart page'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Cart'),
+        ),
+        body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context, index) {
+            final cartItem = cart[index];
+
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                  cartItem['imageUrl'] as String,
+                ),
+                radius: 30,
+              ),
+              title: Text(
+                cartItem['title'] as String,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              subtitle: Text('Size: ${cartItem['size']}'),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
