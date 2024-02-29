57c52fc3b0508287adf4f90ddf2c645934f750b6
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-02-29 11:16:55 +0530

    add the basic starting code for CartPage in cart_page.dart file and update the body of Scaffold in the HomePage to display either the ProductList or CartPage depending on the choice of user in bottomNavigationBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/cart_page.dart';
 import 'package:shopping_app/product_list.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,11 +11,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentPage = 0;
+  List<Widget> pages = const [ProductList(), CartPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const ProductList(),
+      body: pages[currentPage],
       bottomNavigationBar: BottomNavigationBar(
         onTap: (value) {
           setState(() {
